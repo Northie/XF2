8fb5db2a781b4fd27977ab870aaf7760ff0e4ccc
Chris Rutledge <chris+gitlab@xeneco.co.uk>
2014-10-28 11:16:40 +0000

    adding more stuff

---
+++ app/settings/filelist.settings.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace settings;
+
+class fileList {
+    use \utils\traits\singleton;
+    use settings;
+    
+    private $classList = [];
+     
+    private function __construct() {
+        $this->includeFileList();
+    }
+    
+    public function includeFileList() {
+        require 'class-list.static.php';
+        $this->classList = $classlist;        
+    }
+    
+    public function getFileForClass($cls) {
+        return $this->classList[$cls];
+    }
+    
+     
+}
--- app/settings/general.trait.php
+++ app/settings/general.trait.php
@@ -9,6 +9,9 @@ trait general {
     protected function readSettings() {
         $settings = [];
         
+        $settings['XENECO'] = [];
+        $settings['XENECO']['CONTEXT'] = 'DEV';
+        
         $this->settings = $settings;
     }
     
