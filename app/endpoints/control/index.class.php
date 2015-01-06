cd5960c2ebef353faa50750d8e99a067ad41973a
Chris Rutledge <chris+gitlab@xeneco.co.uk>
2015-01-06 13:04:06 +0000

    porting behaviour to control realm

--- app/endpoints/control/index.class.php
+++ app/endpoints/control/index.class.php
@@ -4,4 +4,12 @@ namespace endpoints\control;
 
 class index {
 	use \endpoints\endpoint;
+	
+	public function __construct($request, $response, $filters) {
+		$this->filters = $filters;
+	}
+
+	public function Execute() {
+		$this->data = ['dummy'=>'data'];
+	}
 }
