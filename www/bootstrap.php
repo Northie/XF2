eea1a3fc0be22e0262ad22c1449e246a262befc3
Chris Rutledge <chris+gitlab@xeneco.co.uk>
2015-02-11 17:03:27 +0000

    starting to take configuration out of main framework code

--- www/bootstrap.php
+++ www/bootstrap.php
@@ -2,3 +2,7 @@
 error_reporting(E_ALL &~ E_NOTICE);
 session_start();
 session_regenerate_id();
+
+define('PROJECT_PATH',dirname(__FILE__));
+
+\settings\registry::Load()->set('CONFIG_PATH',  realpath(PROJECT_PATH."/conf/"));
