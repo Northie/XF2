a5acdd2fc4b134cf12224bbf7f3764da6646a9cd
Administrator <chris+gitlab@xeneco.co.uk>
2015-02-17 22:37:21 +0000

    database to read conf files

--- app/settings/database.trait.php
+++ app/settings/database.trait.php
@@ -7,32 +7,7 @@ trait _database {
 	private $settings = [];
 
 	protected function readSettings() {
-		$settings['default'] = [];
-
-		$settings['default']['type'] = 'mysql';
-		$settings['default']['host'] = 'localhost';
-		$settings['default']['user'] = 'chris';
-		$settings['default']['pass'] = 'XQnSPRDs446T9Kfb';
-		$settings['default']['name'] = 'testapp';
-
-		$settings['write'] = [];
-
-		$settings['write']['type'] = 'mysql';
-		$settings['write']['host'] = 'master.db.app'; //bypasses load balancer
-		$settings['write']['user'] = 'writer';
-		$settings['write']['pass'] = 'password';
-		$settings['write']['name'] = 'app';
-
-		$settings['read'] = [];
-
-		$settings['read']['type'] = 'mysql';
-		$settings['read']['host'] = 'lb.db.app'; //connect through load balancer
-		$settings['read']['user'] = 'reader';
-		$settings['read']['pass'] = 'password';
-		$settings['read']['name'] = 'app';
-
-
-		$this->settings = &$settings;
+		$this->settings = parse_ini_file($settings['CONF_DIR'] . "/ini/database.ini", 1);
 	}
 
 }
